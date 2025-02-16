89b7529fdbb397e13afab411f05d2f963acb74cd
yehezkr7 <regevel2006@gmail.com>
2025-02-16 19:28:23 +0200

    hopefully now we could import and use test_em when this repo is pip installed in other repos

--- setup.py
+++ setup.py
@@ -1,50 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#
-#    py-graph-em Graph EM
-#    Copyright (c) 2021 Be The Match operated by National Marrow Donor Program. All Rights Reserved.
-#
-#    This library is free software; you can redistribute it and/or modify it
-#    under the terms of the GNU Lesser General Public License as published
-#    by the Free Software Foundation; either version 3 of the License, or (at
-#    your option) any later version.
-#
-#    This library is distributed in the hope that it will be useful, but WITHOUT
-#    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
-#    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
-#    License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with this library;  if not, write to the Free Software Foundation,
-#    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
-#
-#    > http://www.fsf.org/licensing/licenses/lgpl.html
-#    > http://www.opensource.org/licenses/lgpl-license.php
-#
-
-
-"""The setup script."""
-
+import os
 from setuptools import setup, find_packages
 
-with open("README.md") as readme_file:
-    readme = readme_file.read()
+# Get the absolute path to this directory.
+here = os.path.abspath(os.path.dirname(__file__))
 
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
+# Read the contents of README.md for the long description.
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    readme = f.read()
 
-with open("requirements.txt") as requirements_file:
-    requirements = requirements_file.read().split("\n")
+# Try to read HISTORY.rst; if it doesn't exist, use an empty string.
+try:
+    with open(os.path.join(here, "HISTORY.rst"), encoding="utf-8") as f:
+        history = f.read()
+except FileNotFoundError:
+    history = ""
 
-with open("requirements-tests.txt") as requirements_file:
-    test_requirements = requirements_file.read().split("\n")
+# Read the requirements and test requirements files.
+with open(os.path.join(here, "requirements.txt"), encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+with open(os.path.join(here, "requirements-tests.txt"), encoding="utf-8") as f:
+    test_requirements = f.read().splitlines()
 
 setup(
-    name="py-graph-em",
-    version="0.0.3",
-    author="Pradeep Bashyal",
-    author_email="pbashyal@nmdp.org",
+    name="py-graph-EM",
+    version="0.0.1",
+    author="Regev Yehezkel Imra",
+    author_email="regevel2006@gmail.com",
     python_requires=">=3.8",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
@@ -56,15 +41,18 @@ setup(
         "Programming Language :: Python :: 3.10",
     ],
     description="Graph Based EM",
-    install_requires=requirements,
-    license="LGPL 3.0",
     long_description=readme + "\n\n" + history,
     long_description_content_type="text/markdown",
-    include_package_data=True,
+    install_requires=requirements,
+    license="LGPL 3.0",
     keywords="Graph, EM",
-    packages=find_packages(include=["EM"]),
+    # Explicitly include the 'EM' package and any subpackages.
+    packages=find_packages(include=["EM", "EM.*"]),
+    include_package_data=True,
+    # Include test_em.py as a script so it gets installed in the environment’s bin/ folder.
+    scripts=["test_em.py"],
     test_suite="tests",
     tests_require=test_requirements,
-    url="https://github.com/nmdp-bioinformatics/py-graph-em",
+    url="https://github.com/Regev32/py-graph-em",
     zip_safe=False,
 )
